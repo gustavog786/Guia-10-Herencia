a05d15c7cc0c194cf5fd72479f9c3d28b2d8770f
Gustavo Godoy <gustavog786@gmail.com>
2022-09-19 20:29:40 -0300

    commit ejer01empezado  19/09/22

---
+++ Java-herencia-ejer01/src/principal/PrincipalEjer01.java
@@ -0,0 +1,15 @@
+/*
+
+ */
+package principal;
+
+
+public class PrincipalEjer01 {
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    }
+    
+}
