42f98b1d2cd657900219f7a8bdb65bfc2fcb89ee
Gustavo Godoy <gustavog786@gmail.com>
2022-09-22 13:32:00 -0300

    commit ejer01 extra empezado  22/09/22

---
+++ 03-Ejercicios extra/Java-herencia-ejer01extra/src/entidad/YateDeLujo.java
@@ -0,0 +1,64 @@
+/*
+ En un puerto se alquilan amarres para barcos de distinto tipo. Para cada Alquiler se guarda: el
+nombre, documento del cliente, la fecha de alquiler, fecha de devolución, la posición del
+amarre y el barco que lo ocupará.
+Un Barco se caracteriza por: su matrícula, su eslora en metros y año de fabricación.
+Sin embargo, se pretende diferenciar la información de algunos tipos de barcos especiales:
+• Número de mástiles para veleros.
+• Potencia en CV para barcos a motor.
+• Potencia en CV y número de camarotes para yates de lujo.
+Un alquiler se calcula multiplicando el número de días de ocupación (calculado con la fecha de
+alquiler y devolución), por un valor módulo de cada barco (obtenido simplemente
+multiplicando por 10 los metros de eslora).
+En los barcos de tipo especial el módulo de cada barco se calcula sacando el módulo normal y
+sumándole el atributo particular de cada barco. En los veleros se suma el número de mástiles,
+en los barcos a motor se le suma la potencia en CV y en los yates se suma la potencia en CV y
+el número de camarotes.
+Utilizando la herencia de forma apropiada, deberemos programar en Java, las clases y los
+métodos necesarios que permitan al usuario elegir el barco que quiera alquilar y mostrarle el
+precio final de su alquiler.
+ */
+package entidad;
+
+
+public class YateDeLujo extends Barco{
+    private double potencia;
+    private int camarotes;
+    
+    //constructores
+
+    public YateDeLujo() {
+    }
+
+    public YateDeLujo(double potencia, int camarotes, String matricula, double eslora, int anioFabricacion) {
+        super(matricula, eslora, anioFabricacion);
+        this.potencia = potencia;
+        this.camarotes = camarotes;
+    }
+    
+    //getter and setter
+
+    public double getPotencia() {
+        return potencia;
+    }
+
+    public void setPotencia(double potencia) {
+        this.potencia = potencia;
+    }
+
+    public int getCamarotes() {
+        return camarotes;
+    }
+
+    public void setCamarotes(int camarotes) {
+        this.camarotes = camarotes;
+    }
+    
+    //to string
+
+    @Override
+    public String toString() {
+        return super.toString()+ "YateDeLujo{" + "potencia=" + potencia + ", camarotes=" + camarotes + '}';
+    }
+    
+}
